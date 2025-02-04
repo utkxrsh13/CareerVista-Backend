d579b260d0f7941a5361d70ff963d30aaa004ed5
Utkarsh Tiwari <utkarsht0813@gmail.com>
2025-02-04 18:05:15 +0530

    jee

--- frontend/src/hooks/useGetAllJobs.jsx
+++ frontend/src/hooks/useGetAllJobs.jsx
@@ -1,30 +1,29 @@
 import axios from 'axios';
 import { useDispatch, useSelector } from 'react-redux';
-import { useEffect } from 'react';
 import { setAllJobs } from '@/redux/jobSlice';
 import { JOB_API_END_POINT } from '@/utils/constant';
 
 const useGetAllJobs = () => {
     const dispatch = useDispatch();
     const { searchedQuery } = useSelector(store => store.job);
-    useEffect(() => {
-        const fetchAllJobs = async () => {
-            try {
-                const res = await axios.get(`${JOB_API_END_POINT}/get?keyword=${searchedQuery}`, {
-                    withCredentials: true,
-                    headers: {
-                        'Authorization': `Bearer ${document.cookie.split('token=')[1]}`
-                    }
-                });
-                if (res.data.success) {
-                    dispatch(setAllJobs(res.data.jobs));
+
+    const fetchAllJobs = async () => {
+        try {
+            const res = await axios.get(`${JOB_API_END_POINT}/get?keyword=${searchedQuery}`, {
+                withCredentials: true,
+                headers: {
+                    'Authorization': `Bearer ${document.cookie.split('token=')[1]}`
                 }
-            } catch (error) {
-                console.log(error);
+            });
+            if (res.data.success) {
+                dispatch(setAllJobs(res.data.jobs));
             }
-        };
-        fetchAllJobs();
-    }, [searchedQuery, dispatch]);
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    return fetchAllJobs;
 };
 
 export default useGetAllJobs;
