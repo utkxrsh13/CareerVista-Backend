84a0baa1c9e606913c5fcc360eeb36d842b1b20a
Utkarsh Tiwari <utkarsht0813@gmail.com>
2025-02-06 16:32:52 +0530

    abcddddd

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,6 +1,6 @@
 const BASE_URL = "https://careervistab.onrender.com";
 
-export const USER_API_END_POINT = `${BASE_URL}/user`;
-export const JOB_API_END_POINT = `${BASE_URL}/job`;
-export const APPLICATION_API_END_POINT = `${BASE_URL}/application`;
-export const COMPANY_API_END_POINT = `${BASE_URL}/company`;
+export const USER_API_END_POINT = `${BASE_URL}/api/v1/user`;
+export const JOB_API_END_POINT = `${BASE_URL}/api/v1/job`;
+export const APPLICATION_API_END_POINT = `${BASE_URL}/api/v1/application`;
+export const COMPANY_API_END_POINT = `${BASE_URL}/api/v1/company`;
