79d61b6d588a29f7e03f26ad451df43a637ca1a9
Utkarsh Tiwari <utkarsht0813@gmail.com>
2025-02-06 17:38:45 +0530

    cibdecb

--- backend/utils/db.js
+++ backend/utils/db.js
@@ -1,11 +1,16 @@
 import mongoose from "mongoose";
 
 const connectDB = async () => {
-    try {
-        await mongoose.connect(`${process.env.MONGODB_URI}`);
-        console.log('mongodb connected successfully');
-    } catch (error) {
-        console.log(error);
-    }
+    // try {
+    //     await mongoose.connect(`${process.env.MONGODB_URI}/JobPortal`);
+    //     console.log('mongodb connected successfully');
+    // } catch (error) {
+    //     console.log(error);
+    // }
+    mongoose.connection.on('connected', ()=>{
+        console.log('DataBase Connected')
+      })
+    
+      await mongoose.connect(`${process.env.MONGODB_URI}`)
 }
 export default connectDB;
