e607507450aebcb4549d838cafdaa27fa74460ac
Utkarsh Tiwari <utkarsht0813@gmail.com>
2025-02-06 15:43:04 +0530

    abcd

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
-    origin:'https://career-vista-backend.vercel.app',
+    origin:'*',
     credentials:true
 }
 
--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,4 +1,4 @@
-const BASE_URL = "https://careervista-backend.onrender.com/api/v1";
+const BASE_URL = "http://localhost:8000/api/v1";
 
 export const USER_API_END_POINT = `${BASE_URL}/user`;
 export const JOB_API_END_POINT = `${BASE_URL}/job`;
