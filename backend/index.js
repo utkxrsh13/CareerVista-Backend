843c87924e586d984a6acefd9d44ea56a0804121
Utkarsh Tiwari <utkarsht0813@gmail.com>
2025-02-06 17:50:56 +0530

    FINAL

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
-    origin:'https://career-vista-backend.vercel.app' || 'http://localhost:5173',
+    origin:'http://localhost:5173',
     credentials:true
 }
 
