3f8ae91b3445ad00ccf1aec895d159ea5457fcf9
Utkarsh Tiwari <utkarsht0813@gmail.com>
2025-02-08 16:18:27 +0530

    lessss

--- backend/frontend/tailwind.config.js
+++ backend/frontend/tailwind.config.js
@@ -2,10 +2,10 @@
 module.exports = {
   darkMode: ["class"],
   content: [
-    './pages/**/*.{js,jsx}',
-    './components/**/*.{js,jsx}',
-    './app/**/*.{js,jsx}',
-    './src/**/*.{js,jsx}',
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+    './app/**/*.{js,ts,jsx,tsx}',
+    './src/**/*.{js,ts,jsx,tsx}',
   ],
   prefix: "",
   theme: {
