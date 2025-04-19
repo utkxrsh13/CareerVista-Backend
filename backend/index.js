4198b7358487d165ae1e865291bd160de4802992
Utkarsh Tiwari <153006721+utkxrsh13@users.noreply.github.com>
2025-04-19 23:22:11 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
-    origin:['http://localhost:5173'],
+    origin:['http://localhost:5173' || 'https://new-job-vista-lyart.vercel.app'],
     credentials:true
 }
 
